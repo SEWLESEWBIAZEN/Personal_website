af22906e63c692c5f271075b18d586238598f681
Sewlesew Biazen <sewlesew.biazen@addisbanksc.local>
2024-02-13 09:01:39 +0300

    adding Skills.js file

---
+++ client/src/components/footer/Skills.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className='d-flex flex-wrap link me-3'>
+        <h6>Skills:</h6>
+        <a>Javascript</a>
+        <a>React</a>        
+        <a>Nodejs</a> 
+        <a>Bootstrap</a>
+        <a>CSS3</a>
+        <a>Tailwind CSS</a>
+        
+        <br/>
+        <h6>Additional Skills:</h6>       
+        <a>C# .NET</a>    
+        <a>Git</a>
+        <a>Postman</a>
+        <a>MongoDB</a>
+        <a>MS SQL Server</a>
+        
+
+    </div>
+  )
+}
+
+export default Skills
