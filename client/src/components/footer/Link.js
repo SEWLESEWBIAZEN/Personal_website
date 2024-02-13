4958e2cb581874b8d82efb5b39dfbb66dd6984c2
Sewlesew Biazen <sewlesew.biazen@addisbanksc.local>
2024-02-13 09:00:43 +0300

    adding Link.js file

---
+++ client/src/components/footer/Link.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+
+const Link = () => {
+  return (
+    <div className="d-xs-block d-sm-flex link m-auto">
+      <a>Home </a>
+      <a>Portifolios</a>
+      <a>About Me </a>
+      <a>Contact Me </a>
+      <a>Testimonials</a>
+    </div>
+  );
+};
+
+export default Link;
