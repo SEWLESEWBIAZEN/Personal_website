6c9c34bbe398efa4239c33357724026ef8add001
Sewlesew Biazen <sewlesew.biazen@addisbanksc.local>
2024-02-14 14:26:37 +0300

    Card Item.js modified

--- client/src/components/body/servicesection/CardItem.js
+++ client/src/components/body/servicesection/CardItem.js
@@ -6,7 +6,7 @@ import { ReactTyped } from 'react-typed'
 const CardItem = ({ cards }) => {
     //console.log(cards)
     return (
-        <div className='d-xs-block d-md-flex justify-content-center m-auto card-hover'>
+        <div className='d-xs-block d-md-flex justify-content-center mx-auto mb-3 card-hover'>
             {cards.map((card,index) => (
                 <div key={index}  className='card container m-3 '>
                     <div className='card-image my-3'>
@@ -22,14 +22,14 @@ const CardItem = ({ cards }) => {
                     </div>
                     <div className='card-text card-description'>
                         <p>
-                            {card.description}
-                            
+                            {card.description}                            
                         </p>
                     </div>
+                    <button className='btn btn-success btn-outline-warning'>Learn More...</button>
+                    <a href='https://www.github.com/SEWLESEWBIAZEN' className='text-center mx-auto my-3'>Find on Github</a>
                 </div>
             )
             )}
-
         </div>
     )
 }
