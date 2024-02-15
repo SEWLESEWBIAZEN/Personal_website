7c6247aceb12760479f331489fea06cd933ad57e
Sewlesew Biazen <sewlesew.biazen@addisbanksc.local>
2024-02-15 08:45:49 +0300

    adding a component with data populated

---
+++ client/src/components/body/testimonials/Testimonials.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ericson from "https://images.app.goo.gl/9zajtnzangSJcXW87"
+import ericson2 from "https://images.app.goo.gl/GKZDiJRKbxfE2Cns8"
+import jhon from "./young.jpg"
+ 
+ const tData=[
+    {
+        "picture":{ericson},
+        "name":"Ericson Taw",
+        "description":"there is some text",
+        "star":3
+
+
+    },
+    {
+        "picture":{ericson2},
+        "name":"Ericson Taw",
+        "description":"there is some text",
+        "star":4
+    },
+    {
+        "picture":{jhon},
+        "name":"Jhon Smith",
+        "description":"there is some text",
+        "star":5
+    }
+]
+export default tData;
