981506b375f5ba3ab656fa02008ebe2117c87405
Sewlesew Biazen <sewlesew.biazen@addisbanksc.local>
2024-02-15 11:59:33 +0300

    increasing the maximum width of testimonee container from 45rem to 55 rem

--- client/src/components/body/testimonials/TestimonyText.js
+++ client/src/components/body/testimonials/TestimonyText.js
@@ -4,7 +4,9 @@ import Stars from './Stars'
 const TestimonyText = ({data}) => {
   return (
     <div className='container d-block m-auto text-center'>
-       <h1 className='text-success'>"</h1> <i><h4>{data.description}</h4></i><h1 className='text-success'>"</h1>
+       
+       <h1 className='text-success '>"</h1> <i><h4>{data.description}</h4></i><h1 className='text-success'>"</h1>
+      
         <Stars star={data.star}/>
     </div>
   )
